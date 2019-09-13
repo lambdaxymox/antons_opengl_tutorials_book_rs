c50bbae2fa220bb5e85e1762b45f585990e653d6
LambdaXymox <lambda.xymox@gmail.com>
2019-09-13 18:34:38 -0500

    Remove spurious build file.

--- demos/24_gui_panels/src/gp_vs.glsl
+++ demos/24_gui_panels/src/gp_vs.glsl
@@ -7,5 +7,5 @@ out vec2 st;
 
 void main() {
     st = (vp + 1.0) * 0.5;
-    gl_Position = proj * view * vec4 (vp, 0.0, 1.0); //(10.0 * vp.x, -1.0, 10.0 * -vp.y, 1.0);
+    gl_Position = proj * view * vec4 (vp, 0.0, 1.0);
 }
